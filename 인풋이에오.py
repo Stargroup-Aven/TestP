8d342c99f456f3fbead2be67f251dc81d887b98c
11 <11>
2025-03-17 11:56:41 +0900

    2주차실습

--- 인풋이에오.py
+++ 인풋이에오.py
@@ -2,9 +2,15 @@ num1 = int(input('숫자1 ==>'))
 
 num2 = int (input('숫자2 ==>'))
 
-S_res = num1 + num2
-print (num1, '+', num2 , '=',S_res)
-M_res = num1 - num2
-print (num1, '-', num2 , '=',M_res)
-R_res = num1 * num2
-print (num1, '*', num2 , '=',R_res)
+A_res = num1 + num2
+print (num1, '+', num2 , '=',A_res)
+B_res = num1 - num2
+print (num1, '-', num2 , '=',B_res)
+C_res = num1 * num2
+print (num1, '*', num2 , '=',C_res)
+D_res = num1 / num2
+print (num1, '/', num2 , '=',D_res)
+F_res = num1 % num2
+print (num1, '%', num2 , '=',F_res)
+E_res = num1 ** num2
+print (num1, '**', num2 , '=',E_res)
