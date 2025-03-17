b3e7dccfc1ece250c23ef525da75494ad639a832
11 <11>
2025-03-17 11:43:32 +0900

    괄곱

---
+++ 변수에오.py
@@ -0,0 +1,27 @@
+#변수 선언
+num1 = 100
+num2 = 200
+
+#더하기 연산
+더하기 = num1 + num2
+print (num1, '+', num2, '=', 더하기 )
+
+result = num1 + num2
+
+#빼기 연산
+빼기 = num1 - num2
+print (num1, '-', num2, '=', 빼기 )
+
+#곱연산
+곱 = num1 * num2
+print (num1, 'x', num2, '=', 곱 )
+
+#나누기 연산
+나누기 = num1 / num2
+print (num1, '÷', num2, '=', 나누기 )
+
+#문자열 
+str1 = '헬로'
+str2 = '월드~~~~'
+print (str1 +' '+ str2)
+print (str1 * 10)
